3d7d9a695577f5d356059b9572e74caa63479aca
ujah michael <hrmmajestyuja@gmail.com>
2023-02-16 18:25:22 -0800

    Hello,printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print a c programme with printf
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,/n");
+}
