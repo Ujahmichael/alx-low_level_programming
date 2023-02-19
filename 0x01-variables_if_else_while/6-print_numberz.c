6d0ba1c26bc6706d79343be7b6176db7c091571b
ujah michael <hrmmajestyuja@gmail.com>
2023-02-19 05:55:13 -0800

    All single digit number of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers of base 10
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
