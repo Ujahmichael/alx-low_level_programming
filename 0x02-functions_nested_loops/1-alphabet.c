99ffbe3b225589b6193294a5171bceab372a9d46
ujah michael <hrmmajestyuja@gmail.com>
2023-02-22 02:35:02 -0800

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet -  prints the alphabet in lowercase
+ *
+ * Return: 0
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
