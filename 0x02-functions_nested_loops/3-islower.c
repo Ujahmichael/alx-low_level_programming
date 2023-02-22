5ed6b7f4632b4e0cd1c6eeb0c8ae2c7ce0e01f24
ujah michael <hrmmajestyuja@gmail.com>
2023-02-22 08:47:58 -0800

    correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,14 +6,13 @@
  * Return: 0
 */
 int _islower(int c)
+
 {
-	if (c >= 'a' && c <= 'z')
+
+	if (c >= 97 && c <= 122)
 	{
-		return (1);
+
+	return (1);
 	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+	return (0);
 }
