323fdd4a0d172e5b198658f8ebebbf355dd03de9
ujah michael <hrmmajestyuja@gmail.com>
2023-02-22 03:51:49 -0800

    second commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ *
+ * return: 0
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			putchar(j);
+		}
+	putchar('\n');
+	}
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * islower - print a function that checks for lowercase character
+ *
+ * Return: 0
+*/
+int _islower(int c);
+{
+	if (c >= 97; &&c <= 122;)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	putchar('\n');
+}
