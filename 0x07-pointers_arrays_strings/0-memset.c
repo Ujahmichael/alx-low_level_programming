7d36285d31f1bb5d7a46523ebd284448ec1ec5df
ujah michael <hrmmajestyuja@gmail.com>
2023-03-06 11:42:59 -0800

    task 1 & task 2

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _memset - fills  memory with a constant byte
+* @s: string
+* @b: char
+* @n: unsigned integer
+* Return: a pointer to the memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+
+for (i = 0; i < n; i++)
+s[i] = b;
+
+return (0);
+}
---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _memcpy - copy memory area
+* @dest: this is pointer to the dest array where the content is to be copied
+* @src: this is pointer to the source of data to be copied
+* @n: number of byte to be copied
+* Return:  value of s pointer to dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
+}
