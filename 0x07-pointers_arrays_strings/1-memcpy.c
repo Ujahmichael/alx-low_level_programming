648fe2fcb3571119f37652431c04fbe8e80b7129
ujah michael <hrmmajestyuja@gmail.com>
2023-03-07 00:03:04 -0800

    task1&2

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,13 +9,11 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-unsigned int i = 0;
+unsigned int i;
 
-while (i < n)
-{
+for (i = 0; i < n; i++)
 dest[i] = src[i];
-i++;
-}
+
 dest[i] = '\0';
 return (dest);
 }
