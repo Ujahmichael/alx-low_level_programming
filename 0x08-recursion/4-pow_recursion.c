69100800dfb25ad16044e2768bfa7b5757c13074
ujah michael <hrmmajestyuja@gmail.com>
2023-06-07 21:59:46 -0700

    task4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - print pow
+ * @x: base
+ * @y: exponent
+ * Return: pow
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	else if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
