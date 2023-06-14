a5f16d085620ac3d7bffaf5010b81a511e69920f
ujah michael <hrmmajestyuja@gmail.com>
2023-06-14 04:45:19 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print name of this program
+ * @argv: argument
+ * @argc: argument count
+ * Return: 0
+ */
+int main(int argc, char  **argv)
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
