652bd3d6bf572ad419dccb554f499236d415c1ec
ujah michael <hrmmajestyuja@gmail.com>
2023-06-20 20:00:39 -0700

    define dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,16 +13,14 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- * dog_t -  type of dog structure
- */
 
+/**
+ * dog_t -  typedef of dog structure
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-
 #endif
