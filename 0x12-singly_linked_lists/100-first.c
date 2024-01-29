f400537f457be74dae775c668bb7d82cf539f6f2
ujahmichael <hrmmajestyuja@gmail.com>
2024-01-28 16:20:57 -0800

    2months after project

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,9 @@
 #include "lists.h"
-void first() __attribute__ ((constructor));
+void first(void) __attribute__ ((constructor));
 /**
  * first - print a sentence before mian execute
  */
-void first()
+void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
